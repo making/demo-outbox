cfcc35187bedd7a62a6b1abf864113fabc138da2
Toshiaki Maki <tmaki@vmware.com>
2023-05-29 12:07:20 +0900

    Update

--- src/main/java/com/example/outbox/DemoOutboxApplication.java
+++ src/main/java/com/example/outbox/DemoOutboxApplication.java
@@ -1,21 +1,12 @@
 package com.example.outbox;
 
-import io.micrometer.observation.ObservationRegistry;
-import io.micrometer.observation.aop.ObservedAspect;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoOutboxApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoOutboxApplication.class, args);
-	}
-
-	@Bean
-	public ObservedAspect observedAspect(ObservationRegistry observationRegistry) {
-		return new ObservedAspect(observationRegistry);
 	}
 }
